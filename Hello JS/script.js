69ec9683cd800f0420ef3748ff670499ab3c904a
AanjoAlim <tijoana098@gmail.com>
2026-07-16 17:56:58 -0300

    16/07

--- Hello JS/script.js
+++ Hello JS/script.js
@@ -1,18 +1,31 @@
 var themeBtn = document.getElementById("themeBtn")
-var body = document.body
+var body = document.body;
 var form = document.getElementById("contactForm")
 
-//Alternar tema
-
-themeBtn.addEventListener("click", function(){
+// Alternar tema
+themeBtn.addEventListener("click", function () {
     body.classList.toggle("dark")
     body.classList.toggle("light")
 
     var isDark = body.classList.contains("dark")
-    themeBtn.textContent = isDark ? " 🌔 Tema Escuro" : "☀️ Tema Claro"
+    themeBtn.textContent = isDark ? "🌔 Tema Escuro" : "☀️ Tema Claro"
 })
 
-//validar o formulário
-form.addEventListener("submit", function(e){
+// Validar o formulário
+form.addEventListener("submit", function (e) {
     e.preventDefault()
+
+    var nome = document.getElementById("nome").value.trim();
+    var email = document.getElementById("email").value.trim();
+    var telefone = document.getElementById("telefone").value.trim();
+    var assunto = document.getElementById("assunto").value.trim();
+    var mensagem = document.getElementById("mensagem").value.trim();
+
+    if (!nome || !email || !telefone || !assunto || !mensagem) {
+        alert("Por favor, preencha todos os campos.")
+        return
+    }
+
+    alert("Mensagem enviada com sucesso!")
+    form.reset()
 })
